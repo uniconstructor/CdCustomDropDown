ab4a415ba49d65def5929e500f66dfc662c587b9
uniconstructor <php1602agregator@gmail.com>
2014-04-12 07:20:34 +0400

    First version of custom dropdown list

---
+++ CdCustomDropDown/CdCustomDropDown.php
@@ -0,0 +1,125 @@
+<?php
+
+/**
+ * Yii wrapper for custom dropdown list.
+ * @see http://tympanus.net/codrops/?p=11112
+ * @see http://tympanus.net/Tutorials/CustomDropDownListStyling/index5.html
+ * 
+ * Important: Font Awesome was removed from this plugin for some compatibility reasons, 
+ * because it is already included in YiiBooster
+ */
+class CdCustomDropDown extends CWidget
+{
+    /**
+     * @var array - menu items
+     * Example:
+     * array(
+     *     // menu item with text only
+     *     [0] => array(
+     *         'url'  => 'http://example.com/section1',
+     *         'text' => 'Menu Item',
+     *         'linkOptions' => array('target' => '_blank'),
+     *     ),
+     *     // menu item with text and icon
+     *     [1] => array(
+     *         'url'  => 'http://example.com/section2',
+     *         'text' => 'Menu Item',
+     *         'icon' => '<i class="icon-gear"></i>', // you can also use any custom html here
+     *         'linkOptions' => array('target' => '_blank'),
+     *     ),
+     *     // menu item with custom html content
+     *     [2] => array(
+     *         'url'  => 'http://example.com/section3',
+     *         'html' => '<i class="icon-gear"></i>Menu Item',
+     *         'linkOptions' => array('target' => '_blank'),
+     *     ),
+     * )
+     */
+    public $items;
+    /**
+     * @var bool - include modernizr library or not
+     */
+    public $includeModernizr = false;
+    /**
+     * @var string - html content, displayed when dropdown is not expanded
+     */
+    public $mainContent = '&nbsp;';
+    /**
+     * @var array
+     */
+    public $wrapperOptions = array(
+        'class' => 'wrapper-dropdown-5',
+    );
+    /**
+     * @var array
+     */
+    public $listOptions = array(
+        'class' => 'dropdown dropdown-bootstrap-fix',
+    );
+    
+    /**
+     * @var string
+     */
+    protected $assetUrl;
+    
+    /**
+     * @see CWidget::init()
+     */
+    public function init()
+    {
+        $this->assetUrl = Yii::app()->assetManager->publish(Yii::getPathOfAlias('ext.CdCustomDropDown.assets'));
+        
+        Yii::app()->clientScript->registerCssFile($this->assetUrl.'/css/style.css');
+        Yii::app()->clientScript->registerCssFile($this->assetUrl.'/css/noJS.css');
+        
+        if ( $this->includeModernizr )
+        {// modernizr library is very popular, and can be already included in your project
+            Yii::app()->clientScript->registerScriptFile($this->assetUrl.'/js/modernizr.custom.js');
+        }
+        $this->wrapperOptions['id'] = $this->id;
+    }
+    
+    /**
+     * @see CWidget::run()
+     */
+    public function run()
+    {
+        $this->render('ext.CdCustomDropDown.views.dropdown');
+    }
+    
+    /**
+     * @param array $item 
+     * @return string
+     */
+    protected function getItemContent($item)
+    {
+        $content     = '';
+        $linkOptions = array();
+        $url         = '#';
+        
+        if ( isset($item['linkOptions']) AND is_array($item['linkOptions']) )
+        {
+            $linkOptions = $item['linkOptions'];
+        }
+        if ( isset($item['url']) )
+        {
+            $url = $item['url'];
+        }
+        
+        if ( isset($item['html']) )
+        {// item with custom html content
+            $content = $item['html'];
+        }elseif ( isset($item['text']) AND isset($item['icon']) )
+        {// item with text and icon
+            $content .= $item['icon'].$item['text'];
+        }elseif ( isset($item['text']) AND ! isset($item['icon']) )
+        {// item with text only
+            $content .= $item['text'];
+        }else
+        {// incorrect function argument
+            throw new CException('Error: wrong menu item content');
+        }
+        
+        return CHtml::link($content, $url, $linkOptions);
+    }
+}
---
+++ CdCustomDropDown/views/dropdown.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Dropdown html layout
+ */
+/* @var $this CdCustomDropDown */
+
+echo CHtml::openTag('div', $this->wrapperOptions);
+echo $this->mainContent;
+echo CHtml::openTag('ul', $this->listOptions);
+foreach ( $this->items as $item )
+{
+    echo CHtml::openTag('li');
+    echo $this->getItemContent($item);
+    echo CHtml::closeTag('li');
+}
+echo CHtml::closeTag('ul');
+echo CHtml::closeTag('div');
+?>
+<script type="text/javascript">
+	function DropDown(el) {
+		this.dd = el;
+		this.initEvents();
+	}
+	
+	DropDown.prototype = {
+		initEvents : function() {
+			var obj = this;
+			obj.dd.on('click', function(event){
+				$(this).toggleClass('active');
+				event.stopPropagation();
+			});	
+		}
+	}
+	
+	$(function() {
+		var dd = new DropDown( $('#<?= $this->id; ?>') );
+		$(document).click(function() {
+			// all dropdowns
+			$('.wrapper-dropdown-5').removeClass('active');
+		});
+	});
+</script>
